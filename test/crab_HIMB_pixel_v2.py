45854bd924b8552be0503fb04dc3b900a68251c5
Quan Wang <BetterWang@gmail.com>
2016-09-23 04:06:46 +0200

    [!]

--- test/crab_HIMB_pixel_v2.py
+++ test/crab_HIMB_pixel_v2.py
@@ -5,7 +5,7 @@ from httplib import HTTPException
 
 config = config()
 
-config.General.requestName = 'HIMB2_PCA_pixel_noeff_v5'
+config.General.requestName = 'HIMB2_PCA_pixel_noeff_v6'
 config.General.workArea = 'CrabArea'
 config.General.transferOutputs = True
 config.General.transferLogs = True
